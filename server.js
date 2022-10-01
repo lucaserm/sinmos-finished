95d7e0b79d69712c8a1921685c4bc4fdfe3e2b67
Lucas Macedo <lucasedumacedo@gmail.com>
2022-10-01 08:16:40 -0400

    master

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.set('view engine', 'ejs');
 app.use(routes);
 
 app.on('Pronto.', () => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log('Acessar em https://localhost:3000');
         console.log('Servidor rodando na porta 3000');
     });
